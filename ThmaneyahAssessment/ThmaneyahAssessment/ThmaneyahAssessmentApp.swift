3a7789453c4022e1720e173078ceb4582b4a1ee0
Maram Alghanoom <malghnoum@sdaia.gov.sa>
2025-07-26 15:00:21 +0300

    create project

---
+++ ThmaneyahAssessment/ThmaneyahAssessment/ThmaneyahAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmaneyahAssessmentApp.swift
+//  ThmaneyahAssessment
+//
+//  Created by Maram Alghanoom on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmaneyahAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThmaneyahAssessment/ThmaneyahAssessmentTests/ThmaneyahAssessmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  ThmaneyahAssessmentTests.swift
+//  ThmaneyahAssessmentTests
+//
+//  Created by Maram Alghanoom on 26/07/2025.
+//
+
+import Testing
+
+struct ThmaneyahAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
